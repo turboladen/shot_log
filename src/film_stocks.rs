001a000b36fbfa3e765416b4fbb0af92b578480d
Steve Loveless <steve@agrian.com>
2017-10-11 21:24:50 -0700

    Associate brands to film stocks

--- src/film_stocks.rs
+++ src/film_stocks.rs
@@ -1,6 +1,6 @@
 use rocket_contrib::Json;
 use rocket_contrib::Template;
-use diesel::LoadDsl;
+use diesel::{ExpressionMethods, JoinDsl, JoinOnDsl, LoadDsl};
 use db_conn::DbConn;
 use models::*;
 use schema::*;
@@ -8,7 +8,13 @@ use schema::*;
 #[derive(Serialize)]
 struct TemplateContext {
     name: String,
-    items: Vec<FilmStock>
+    film_stocks: Vec<FullFilmStock>
+}
+
+#[derive(Serialize)]
+struct FullFilmStock {
+    film_stock: FilmStock,
+    brand: Brand,
 }
 
 #[get("/film_stocks", format = "application/json")]
@@ -21,12 +27,18 @@ fn index_json(conn: DbConn) -> Json<Vec<FilmStock>> {
 
 #[get("/film_stocks", format = "text/html")]
 fn index_html(conn: DbConn) -> Template {
-    let result = film_stocks::table.load::<FilmStock>(&*conn);
-    let stocks = result.expect("Error loading film_stocks");
+    let result = film_stocks::table
+        .inner_join(brands::table.on(film_stocks::brand_id.eq(brands::id)))
+        .load(&*conn);
+    let stocks: Vec<(FilmStock, Brand)>  = result.expect("Error loading film_stocks");
+
+    let full_stocks = stocks.into_iter()
+        .map(|(fs, b)| FullFilmStock { film_stock: fs, brand: b })
+        .collect();
 
     let context = TemplateContext {
         name: "Film Stocks".to_string(),
-        items: stocks,
+        film_stocks: full_stocks,
     };
 
     Template::render("film_stocks/index", context)
--- src/models.rs
+++ src/models.rs
@@ -1,12 +1,13 @@
 use uuid::Uuid;
+use schema::{brands, film_formats, film_stocks};
 
-#[derive(Queryable, Serialize)]
+#[derive(Identifiable, Queryable, Serialize, Associations)]
 pub struct Brand {
     pub id: Uuid,
     pub name: String,
 }
 
-#[derive(Queryable, Serialize)]
+#[derive(Identifiable, Queryable, Serialize, Associations)]
 pub struct FilmFormat {
     pub id: Uuid,
     pub designation: String,
@@ -14,7 +15,9 @@ pub struct FilmFormat {
     pub stock_size_unit: Option<String>,
 }
 
-#[derive(Queryable, Serialize)]
+#[derive(Identifiable, Queryable, Serialize, Associations)]
+#[belongs_to(Brand)]
+#[belongs_to(FilmFormat)]
 pub struct FilmStock {
     pub id: Uuid,
     pub box_name: String,
